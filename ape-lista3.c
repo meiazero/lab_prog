94d7a475d27fdb666c6602abc3244242f1b83ce4
Emanuel Avila <76269418+meiazero@users.noreply.github.com>
2022-05-21 11:45:44 -0300

    questao 1

--- ape-lista3.c
+++ ape-lista3.c
@@ -1,10 +1,65 @@
 #include <stdio.h>
 #include <math.h>
+#include <ctype.h>
 #include <locale.h>
 
 
 int questao1(){
+    //idade,cep
+    int old, zipcode;
+    //peso, altura
+    float weight, height;
+    //nome,sexo, trabalho, rua, bairro, estado, cidade. 
+    char name[20], sex[1], job[12], street[30], district, city;
+    
+    fflush(stdin);
+    printf("Qual seu nome ");
+    scanf("%s", &name);
+    if(name[20] =! ""){
+        fflush(stdin);
+        printf("Qual seu sexo: ");
+        scanf("%c", &sex);
+        sex[0] = toupper(sex[0]);
+        if(sex[0] == 'M' || 'F'){
+            fflush(stdin);
+            printf("Qual sua idade: ");
+            scanf("%d", &old);
+            if(old =! ""){
+                fflush(stdin);
+                printf("Qual sua profissao: ");
+                scanf("%s", &job);
+                if(job[0] =! ""){
+                    fflush(stdin);
+                    printf("Qual seu peso: ");
+                    scanf("%f", &weight);
+                    if(weight =! ""){
+                        fflush(stdin);
+                        printf("Qual sua altura: ");
+                        scanf("%f", &height);
+                        if(height =! ""){
+                            fflush(stdin);
+                            printf("Qual o nome da sua Rua: ");
+                            scanf("%s", &street);
+                            if(street[0] =! ""){
+                                fflush(stdin);
+                                printf("Qual o nome do seu bairro: ");
+                                scanf("%s", &district);
+                                if(district =! ""){
+                                    fflush(stdin);
+                                    printf("Qual o nome da sua cidade: ");
+                                    scanf("%s", &city);
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }else{
+            printf("Sexo invalido");
+        }
+    }
 
+    return 0;
 }
 
 int questoes(){
@@ -12,8 +67,9 @@ int questoes(){
     int questao, k = 0;
 
     printf("\n Escolha uma questao: \n\n");
-    printf("1 - Faça um programa em C que leia um número inteiro e determine se ele é par ou ímpar\n\n");
-        printf(" 0 - Sair\n\n");
+    //lista de questoes.
+    printf("1 - Faça um algoritmo para ler as seguintes informações de uma pessoa:\nNome, Idade, Sexo, Peso, Altura, Profissão, Rua, Bairro, Cidade, Estado, CEP, Telefone.\n");
+    printf(" 0 - Sair\n\n");
     
     printf("\n\nDigite a questao -> ");
     scanf("%d", &questao);
@@ -26,7 +82,8 @@ int questoes(){
                 break;
             case 1:
                 questao1();
-                        default:
+                break;
+            default:
                 printf("\n\nQuestao inválida!\n\n");
         	}
 	
