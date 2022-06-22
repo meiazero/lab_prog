9fddcfe5b64923f8b65fd37197a56e16a07ca011
meiazero <avilaemanueel@gmail.com>
2022-06-22 01:02:17 -0300

    calculadora

--- calculadora.c
+++ calculadora.c
@@ -1,31 +1,84 @@
 #include <stdio.h>
 
-// int raiz(int v1, int v2){
-//     return;   
-// }
-// int potencia(int v1, int v2){
-//     return;   
-// }
-// float subtracao(float v1, float v2){
-//     return;  
-// }
-// float multiplicacao(float v1, float v2){
-//     return;   
-// }
-// float subtracao(float v1, float v2){
-//     return;  
-// }
-// float soma(float v1, float v2){
-//     return;
-// }
+int raiz(float v1, float v2){
+    float result;
+
+    return printf("o resultado e %2.3f\n", result);   
+}
+int potencia(float v1, float v2){
+    float result, rest;
+    int i;
+    
+    if(v2 == 0){
+        result = 1;
+    }else{
+        printf("Valor do v1: %2.1f\n", v1);
+        printf("Valor do v2: %2.1f\n", v2);
+        // i = v2;
+        for(i = 1; i <= v2; i++){ 
+            result = ;
+        }
+    }
+    
+    return printf("o resultado e %2.3f\n", result);   
+}
+float divisao(float v1, float v2){
+    float result;
+
+    result = v1 / v2;
+    return printf("o resultado e %2.3f\n", result);  
+}
+float multiplicacao(float v1, float v2){
+    float result;
+
+    result = v1 * v2; 
+    return printf("o resultado e %2.3f\n", result);   
+}
+float subtracao(float v1, float v2){
+    float result;
+
+    result = v1 - v2;
+    return printf("o resultado e %2.3f\n", result);  
+}
+float soma(float v1, float v2){
+    float result;
+
+    result = v1 + v2;
+    return printf("o resultado e %2.3f\n", result);
+}
 void display(){
     float number1, number2;
     char simbolo;
 
     printf("Digite o calculo que quer realizar: ");
     scanf("%f %c %f", &number1, &simbolo, &number2);
-
-    printf("operacao feita foi de: %c", simbolo);
+    // printf("=======> operacao feita foi de: %c\n", simbolo);
+    switch (simbolo){
+        case '+':
+            soma(number1, number2);
+            break;
+        case '-':
+            subtracao(number1, number2);
+            break;
+        case '*':
+            multiplicacao(number1, number2);
+            break;
+        case 'x':
+            multiplicacao(number1, number2);
+            break;
+        case '.':
+            multiplicacao(number1, number2);
+            break;
+        case '^':
+            potencia(number1, number2);
+            break;
+        case '/':
+            divisao(number1, number2);
+        default:
+            printf("erro!");
+            break;
+    }
+    
     
     return;
 }
