6c0ebafe1e219f84e61ba03a8424b72c77b9135e
meiazero <avilaemanueel@gmail.com>
2022-06-22 14:28:56 -0300

    questão 1 finalizada.

--- funcoes/q1.c
+++ funcoes/q1.c
@@ -1,22 +1,29 @@
 #include <stdio.h>
+#include <math.h>
 
-float calc(float n1, int n2);
+float calc(float n1, float n2);
 
 int main(){
-    float x, y;
+    float x, y, r1, r2, r3, rf;
     int a, b;
 
-    // printf("");
+    
     printf("Digite os valores reais: ");
     scanf("%f %f", &x, &y);
     printf("Digite os valores inteiros: ");
     scanf("%i %i", &a, &b);
+    r1 = calc(x, a);
+    r2 = calc(y, b);
+    r3 = calc((x-y),(a+b));
+    rf = r1 + r2 + r3;
+    
+    printf("O resultado de X^a + Y^b + (X - Y)^(a + b) e: %2.2f", rf);
 
     return 0;
 }
-float calc(float n1, int n2){
+float calc(float n1, float n2){
     float res;
+    
     res = pow(n1, n2); 
-
     return res;
 }
