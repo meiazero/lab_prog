1a2793675419e11424cd803359126efefd0aef47
meiazero <avilaemanueel@gmail.com>
2022-06-22 15:17:25 -0300

    bug fix

--- funcoes/q1.c
+++ funcoes/q1.c
@@ -17,13 +17,13 @@ int main(){
     r3 = calc((x-y),(a+b));
     rf = r1 + r2 + r3;
     
-    printf("O resultado de X^a + Y^b + (X - Y)^(a + b) e: %2.2f", rf);
+    printf("O resultado de X^a + Y^b + (X - Y)^(a + b) e: %2.2f\n", rf);
 
     return 0;
 }
 float calc(float n1, float n2){
     float res;
-    
+
     res = pow(n1, n2); 
     return res;
 }
